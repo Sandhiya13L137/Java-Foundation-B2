dbfe638f1af8811ec47da06bcd3e243418ff501f
msgtovp <msgtovp@gmail.com>
2016-11-10 14:49:53 +0530

    Create EnhacedForLoop.java

---
+++ L01-Fundamentals-Operators-FlowControlStatements/EnhacedForLoop.java
@@ -0,0 +1,8 @@
+public class EnhacedForLoop {
+    static public void main(String ...args) {
+        System.out.println("Arguments: ");
+        for(String each:args) {
+            System.out.println(each);
+        }
+    }
+}
