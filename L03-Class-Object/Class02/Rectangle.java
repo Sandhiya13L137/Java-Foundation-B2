a9c636300a1bdc267fe08f0fb6c3b2b272f0ace9
msgtovp <msgtovp@gmail.com>
2016-11-12 15:35:27 +0530

    Create Rectangle.java

---
+++ L03-Class-Object/Class02/Rectangle.java
@@ -0,0 +1,7 @@
+public class Rectangle {
+    double breadth;
+    double length;
+    void area() {
+        System.out.println(String.format("AREA OF RECTANGLE IS %.2f", (length * breadth)));
+    }
+}
