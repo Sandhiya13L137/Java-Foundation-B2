46298df93f0663693876b47fe5d8b79961a6d5f7
msgtovp <msgtovp@gmail.com>
2016-11-12 15:34:30 +0530

    Create Circle.java

---
+++ L03-Class-Object/Class02/Circle.java
@@ -0,0 +1,7 @@
+public class Circle {
+    double radius;
+    static double pi = 3.14;
+    void area() {
+        System.out.println(String.format("AREA OF CIRCLE IS %.2f",(radius * radius * pi)));
+    }
+}
