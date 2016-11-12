d18445c00cb2ed939bbd40e668441e077410850b
msgtovp <msgtovp@gmail.com>
2016-11-12 15:34:58 +0530

    Create Triangle.java

---
+++ L03-Class-Object/Class02/Triangle.java
@@ -0,0 +1,7 @@
+public class Triangle{
+    double base;
+    double height;
+    void area(){
+        System.out.println(String.format("AREA OF TRIANGLE IS %.2f ", (0.5 * base * height)));
+    }
+}
